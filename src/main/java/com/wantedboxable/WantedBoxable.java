c42bfce17406ef2376ff6e6b72101df576a66c1f
CrossVas <cross.wanted.1.4@gmail.com>
2023-11-15 14:22:07 +0900

    revived after a long break

--- src/main/java/com/wantedboxable/WantedBoxable.java
+++ src/main/java/com/wantedboxable/WantedBoxable.java
@@ -1,26 +1,39 @@
 package com.wantedboxable;
 
 import ic2.core.platform.recipes.helpers.Boxables;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModLoadingContext;
+import ic2.core.utils.config.Config;
+import ic2.core.utils.config.ConfigEntry;
+import ic2.core.utils.config.ConfigHandler;
+import ic2.core.utils.config.ConfigSection;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod("wantedboxable")
+import java.util.Arrays;
+
+@Mod(WantedBoxable.ID)
 public class WantedBoxable {
 
+    public static final String ID = "wantedboxable";
+
     public WantedBoxable() {
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, BoxableConfig.spec, "wantedboxable.toml");
-        bus.addListener(this::commonLoad);
+        WantedBoxableConfig.initConfig();
+        Arrays.stream(WantedBoxableConfig.PREFIXES.get()).forEach(entry -> {
+            if (!entry.isEmpty()) {
+                Boxables.INSTANCE.registerPrefix(entry);
+            }
+        });
+
     }
 
-    public void commonLoad(FMLCommonSetupEvent e) {
-        for (String prefix : BoxableConfig.PREFIXES.get()) {
-            if (!prefix.isEmpty())
-                Boxables.INSTANCE.registerPrefix(prefix);
+    public static class WantedBoxableConfig {
+        public static ConfigHandler HANDLER;
+        public static ConfigEntry.ArrayValue PREFIXES;
+
+        public static void initConfig() {
+            Config CONFIG = new Config(WantedBoxable.ID);
+            ConfigSection GENERAL = CONFIG.add("general");
+            PREFIXES = GENERAL.addArray("boxable", "A list of wanted boxable registry names").set(new String[] {"lexicon", "mana_tablet"}).setGameRestart();
+            HANDLER = new ConfigHandler("ic2c", CONFIG, false);
+            HANDLER.init();
         }
     }
 }
